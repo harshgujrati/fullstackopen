ccbef1442995c5e6280d135a2bc2f265ef5d4b56
harshgujrati <harsh19may@gmail.com>
2022-04-02 11:24:39 +0530

    retrive notes from db.json using axios and effect hooks

--- part2/2.6_to_2.10-the-phonebook/src/App.js
+++ part2/2.6_to_2.10-the-phonebook/src/App.js
@@ -1,23 +1,23 @@
-import { useState } from 'react'
-
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Persons from './Persons'
 import Filter from './Filter'
 import PersonForm from './PersonForm'
 
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
-
-
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newSearch, setNewSearch] = useState('')
   const [newNumber, setNewNumber] = useState('')
 
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
   const handleForm = (event) => {
     event.preventDefault()
     
